a36cf6ff86638e12b8a7fc1b8d5c1fcafacbbc63
Anderson Araujo <91352623+i-am-anderson@users.noreply.github.com>
2025-09-25 00:42:55 -0300

    Feature/shadcn UI (#2)
    * feat: added shadcn ui
    
    * feat: create forms with shadcnui and tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,53 @@
-import Image from "next/image";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect, RedirectType } from "next/navigation";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import CreateAccountForm from "@/components/auth/create-account-form";
+import LoginAccountForm from "@/components/auth/login-account-form";
 
-export default function Home() {
-  return <div>Home</div>;
+export default async function Home() {
+  let loggedIn = false;
+
+  try {
+    const supabase = createServerComponentClient({ cookies });
+    const {
+      data: { session },
+    } = await supabase.auth.getSession();
+
+    if (session) loggedIn = true;
+  } catch (error) {
+    console.error("Home", error);
+  } finally {
+    if (loggedIn) redirect("/user-app", RedirectType.replace);
+  }
+
+  return (
+    <div className="flex flex-col h-screen w-full justify-center items-center">
+      <Tabs
+        defaultValue="create-account"
+        className="w-[400px] border rounded-md pb-4 shadow-2xl"
+      >
+        <TabsList className="flex justify-around items-center rounded-b-none w-full h-14">
+          <TabsTrigger
+            value="create-account"
+            className="cursor-pointer transition-all delay-150"
+          >
+            Create Account
+          </TabsTrigger>
+          <TabsTrigger
+            value="login"
+            className="cursor-pointer transition-all delay-150"
+          >
+            Login
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="create-account">
+          <CreateAccountForm />
+        </TabsContent>
+        <TabsContent value="login">
+          <LoginAccountForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
---
+++ components/auth/create-account-form.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+// Schema
+const formSchema = z.object({
+  email: z
+    .email({
+      pattern:
+        /^(?!\.)(?!.*\.\.)([a-z0-9_'+\-\.]*)[a-z0-9_+-]@([a-z0-9][a-z0-9\-]*\.)+[a-z]{2,}$/i,
+      error: "E-mail is invalid.",
+    })
+    .nonempty("E-mail is required."),
+  password: z
+    .string()
+    .nonempty("Password is required.")
+    .min(7, { error: "Password must have a least 7 characters" })
+    .min(12),
+});
+
+export default function CreateAccountForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center space-y-2">
+      <span className="text-lg">You will love it.</span>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail</FormLabel>
+                <FormControl>
+                  <Input placeholder="E-mail" {...field} />
+                </FormControl>
+                <FormDescription>This is your e-mail</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" {...field} />
+                </FormControl>
+                <FormDescription>This is your password</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Create Account</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
